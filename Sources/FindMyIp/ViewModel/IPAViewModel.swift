8f66f066fcc746001ddc242f14acd0e6db617dc0
Muneerahmmad-MM <110410536+Muneerahmmad-MM@users.noreply.github.com>
2023-12-06 17:10:02 +0530

    fix: updated access level

--- Sources/FindMyIp/ViewModel/IPAViewModel.swift
+++ Sources/FindMyIp/ViewModel/IPAViewModel.swift
@@ -33,7 +33,7 @@ public class IPAViewModel<T>: ObservableObject where T: NetworkServiceProvider {
         }
     }
     
-    private func errorHandler(error: NetworkError) {
+    public func errorHandler(error: NetworkError) {
         if let backendError = error.backendError {
             errorDescription = backendError.message
         } else {
